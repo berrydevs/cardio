dfafb9280f62fd730d5d43566aeca1d406a35ce3
Tamir Azrab <tamirazrab@gmail.com>
2021-12-23 16:49:33 +0500

    feat: :sparkles: add homepage & footer

---
+++ src/components/Container/container.js
@@ -0,0 +1,10 @@
+function Container({ children, className, ...rest }) {
+  let containerClassName = styles.container
+
+  if (className) {
+    containerClassName = `${containerClassName} ${className}`
+  }
+  return (<div className={containerClassName} {...rest}>{children}</div>)
+}
+
+export default Container
---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import styles from '../Header/Header.module.scss'
+import Container from '@components/Container'
+import { FaShoppingCart } from 'react-icons/fa'
+function Header() {
+  return (<header className={styles.header}>
+    <Container className={styles.headerContainer}>
+      <p className={styles['header-title']}>Cardio Store</p>
+      <p className={styles.headerCart}>
+        <button>
+          <FaShoppingCart />
+          <span>
+            $0.00
+          </span>
+        </button>
+      </p>
+    </Container>
+  </header>)
+}
+
+export default Header
